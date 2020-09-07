288c1c4b4364db055d82a2521e4e02182dec6432
Scotti <sday@next.tech>
2020-09-07 19:33:45 -0400

    Deliverable 1, step 48

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,5 +1,14 @@
 var controllerOptions = {};
+var i = 0;
+
+// width
+var x = window.innerWidth/2;
+// height
+var y = window.innerHeight/2;
+
 Leap.loop(controllerOptions, function(frame){
-console.log(i)
+    console.log(i)
+    i++
+    circle(x,y,50)
 }
 );
